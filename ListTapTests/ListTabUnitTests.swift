ea06a0c2b72ac2a160cde4ce40cb602cba163004
VasilkovaIvanna <ivanna.vasilkova@wolkov.consulting>
2022-10-14 14:03:51 +0200

    Init commit

---
+++ ListTapTests/ListTabUnitTests.swift
@@ -0,0 +1,50 @@
+//
+//  ListTabUnitTests.swift
+//  ListTapUITests
+//
+//  Created by Ivy on 16.09.2022.
+//
+
+import XCTest
+@testable import ListTap
+
+class ListTabUnitTests: XCTestCase {
+    
+    var sut = ListViewModel()
+
+    override func setUpWithError() throws {
+        su
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testAppendRandomElement() throws {
+        sut.a
+    }
+    
+    func testIncrementRandomElement() throws {
+    
+    }
+    
+    func testResetRandomElementCounter() throws {
+    
+    }
+    
+    func testDeleteRandomElement() throws {
+    
+    }
+    
+    func testAddValueOfAnElementBefore() throws {
+    
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
